c7f26e40cc2279ec2cf770b61235ef26ee6fb494
gzp <->
2023-04-12 15:25:44 +0200

    add network monitor

--- Assets/Game/Scripts/GameManager.cs
+++ Assets/Game/Scripts/GameManager.cs
@@ -1,15 +1,38 @@
+using System.Net;
+using System.Net.Sockets;
 using Unity.Netcode;
+using Unity.Netcode.Transports.UTP;
 using UnityEngine;
 
 public class GameManager : MonoBehaviour
 {
     public bool ShowDebugHUD = true;
-    public GameObject touchCanvas;
+    public string HostIP;
+
+    private GameObject touchCanvas;
+
+    public static string GetLocalIPAddress()
+    {
+        try
+        {
+            var host = Dns.GetHostEntry(Dns.GetHostName());
+            foreach (var ip in host.AddressList)
+            {
+                if (ip.AddressFamily == AddressFamily.InterNetwork)
+                {
+                    return ip.ToString();
+                }
+            }
+        }
+        catch { }
+        return "127.0.0.1";
+    }
 
     void Start()
     {
         touchCanvas = NamedGameObjects.TouchCanvas;
         touchCanvas.SetActive(false);
+        HostIP = GetLocalIPAddress();
     }
 
     private void OnGUI()
@@ -19,20 +42,25 @@ public class GameManager : MonoBehaviour
         var networkManager = NetworkManager.Singleton;
         if (!networkManager.IsClient && !networkManager.IsServer)
         {
+            HostIP = GUILayout.TextField(HostIP, 25);
+
             if (GUILayout.Button("Host"))
             {
+                networkManager.GetComponent<UnityTransport>().ConnectionData.Address = HostIP;
                 networkManager.StartHost();
                 touchCanvas.SetActive(true);
             }
 
             if (GUILayout.Button("Client"))
             {
+                networkManager.GetComponent<UnityTransport>().ConnectionData.Address = HostIP;
                 networkManager.StartClient();
                 touchCanvas.SetActive(true);
             }
 
             if (GUILayout.Button("Server"))
             {
+                networkManager.GetComponent<UnityTransport>().ConnectionData.Address = HostIP;
                 networkManager.StartServer();
             }
         }
